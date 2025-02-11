490c282d1687efd5dd91f2f450e505410a73d42e
arcanel4bs <information.arcane@gmail.com>
2025-02-10 20:49:02 -0500

    layout name change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>ASIMOV</title>
+        <title>Noesis | Research Agent Assistant</title>
       </head>
       <body className={spaceGrotesk.className}>{children}</body>
     </html>
